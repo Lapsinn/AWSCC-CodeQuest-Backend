3c43d07f86756730ae5c21826a097a0165c6f4c2
Lapsinn <142302012+Lapsinn@users.noreply.github.com>
2023-11-20 18:50:49 +0800

    day1 v1

---
+++ backend/day-01/day1.py
@@ -0,0 +1,12 @@
+def main():
+    your_name = "Aldwin"
+    name_of_department = "Department of Software and Web Development"
+    about_you = ("I am a passionate person, but I can be very clumsy and disorganized at times."
+                 " So I may miss some meetings due to my own fault but I'll still do my best with"
+                 " every opportunity given to me. So thank you very much for having me.!!!")
+
+    print(f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!",
+          f"One thing about me is that {about_you}.")
+
+if __name__ == '__main__':
+    main()
