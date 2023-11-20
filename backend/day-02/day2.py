464e914a65894fb7d7105017a067a7f620ffc02b
Lapsinn <142302012+Lapsinn@users.noreply.github.com>
2023-11-20 19:00:54 +0800

    day2 v1

---
+++ backend/day-02/day2.py
@@ -0,0 +1,12 @@
+def main():
+    first = int(input("Enter the first number: "))
+    second = int(input("Enter the second number: "))
+    third = int(input("Enter the third number: "))
+
+    sum = first + second + third
+
+    print(f"Sum: {sum}")
+
+if __name__ == '__main__':
+    main()
+
