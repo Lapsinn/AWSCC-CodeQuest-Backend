d96f57b5533aa15ba38d9a615792399fa08168f0
Lapsinn <142302012+Lapsinn@users.noreply.github.com>
2023-11-24 15:48:36 +0800

    added random note and corrected a typo

--- submissions/day_09.py
+++ submissions/day_09.py
@@ -1,5 +1,6 @@
 from area import m_input, circle
 import sys
+from random import randint
 
 def main():
     radius = m_input("Radius: ")
@@ -9,12 +10,17 @@ def main():
         try:
             area = round(area, int(sys.argv[1]))
         except ValueError:
-            print("Invalid int for rounf off.")
+            print("Invalid int for round off.")
 
     if len(radius) == 2:
         print(area, radius[1])
     else:
         print(area)
+
+    random = randint(1, 10)
+    if random == 7 and len(sys.argv) != 2:
+        print("You can round off the decimal to your preferred number of digits\n by inserting "
+              "a single int command-line argument which uses the round() function")
 
     sys.exit(0)
 
